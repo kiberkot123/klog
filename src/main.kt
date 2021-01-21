2cdee7cb28f8c6bdcafd1f2ab59669f6512c70be
Zainds <vasiliyjoskiy@gmil.com>
2021-01-21 18:59:40 +0700

    1

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,21 @@
+import java.time.temporal.TemporalAccessor
+
 fun main()
 {
-    var a = 1000
-    var b = 1
-    print(a+b)
+
+
+
+
+    print("Введите скорость первого автомобиля:")
+    var V1 = readLine()!!.toDouble()
+    print("Введите скорость второго автомобиля:")
+    var V2 = readLine()!!.toDouble()
+    print("Введите расстояние на момент начала рассчета")
+    var S_begin = readLine()!!.toDouble()
+    print("Время прошедшее со старта:")
+    var T = readLine()!!.toDouble()
+    var s_final =  S_begin+V1*T+V2*T
+    print("Итоговый рассчет: $S_final")
 ////
 /////
 
