00271a1f748453b85ab03e6dbb86ea12c1d9f5ab
Zainds <vasiliyjoskiy@gmil.com>
2021-01-26 18:27:00 +0700

    11122

--- src/main.kt
+++ src/main.kt
@@ -1,24 +1,13 @@
-import java.time.temporal.TemporalAccessor
+import kotlin.random.Random
 
 fun main()
 {
+   var a = Random.nextInt(1,8)
 
 
-
-
-    print("Введите скорость первого автомобиля:")
-    var V1 = readLine()!!.toDouble()
-    print("Введите скорость второго автомобиля:")
-    var V2 = readLine()!!.toDouble()
-    print("Введите расстояние на момент начала рассчета")
-    var S_begin = readLine()!!.toDouble()
-    print("Время прошедшее со старта:")
-    var T = readLine()!!.toDouble()
-    var s_final =  S_begin+V1*T+V2*T
-    print("Итоговый рассчет: $S_final")
-////
-/////
 
 
 
 }
+
+
